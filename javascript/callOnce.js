ab81073979e3dfb5fdc8ee962516d19284c102a1
txu89 <xudotcom@gmail.com>
2017-04-24 18:38:04 -0400

    fixed callOnce closure
    callOnce is now correctly only running toBeCalled one time.

--- javascript/callOnce.js
+++ javascript/callOnce.js
@@ -1,21 +1,26 @@
+/**
+ * Closure
+ *
+ * @param  {function} toBeCalled
+ * @return {function}
+ */
 function callOnce(toBeCalled) {
-  var called = false;
-  if (called === true){
-    return;
-  }
-else {
-  called = true;
-  }
-   
-  return toBeCalled();
+    var called = false;
+    
+    return function() {
+        if (called === false) {
+            toBeCalled();
+        }
+        called = true;
+    }
 }
 
 function greet() {
-  console.log("Hey!");
-  }
+    console.log("Hey!");
+}
 
 var callOnceGreet = callOnce(greet);
-callOnceGreet;
-callOnceGreet;
-callOnceGreet;
-callOnceGreet;
+callOnceGreet();
+callOnceGreet();
+callOnceGreet();
+callOnceGreet();
