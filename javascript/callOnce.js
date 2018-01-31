9877fc939f7284545ca342f22e978633851b7778
Tom Xu <xudotcom@gmail.com>
2018-01-31 15:16:08 +0900

    cleaned up callOnce function

--- javascript/callOnce.js
+++ javascript/callOnce.js
@@ -5,19 +5,15 @@
  * @return {function}
  */
 function callOnce(callback) {
-    // isCalled, should-, has-, shouldDisplay, shouldValidate
-    // hasErrors, hasBeenCalled, isDisabled
-    var isCalled = false;
+
+    var isCalled = 0;
 
     return function() {
-        /**
-         * if (!isCalled)
-         * could be a zero, could be string !0 eval
-         */
-        if (isCalled === false) {
+
+        if (!isCalled) {
             callback();
         }
-        isCalled = true;
+        isCalled = 1;
     }
 }
 
@@ -30,13 +26,3 @@ callOnceGreet();
 callOnceGreet();
 callOnceGreet();
 callOnceGreet();
-
-// What to name the returned function
-// Style: naming, visual asthetics
-// clear and consistent, work with a lot of people looks the same
-
-console.log(eval(!0));
-console.log(eval(!''));
-
-// Tell me the difference between statically-typed and dynamically-languages.
-// Try to line width to less than 80. 72 is extremists.
